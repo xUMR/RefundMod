46ede0b5c1125eabee6e885ec5d9a8caa7679e37
xUMR <umurcem20@gmail.com>
2015-03-26 22:20:58 +0200

    Initial commit

---
+++ RefundMod/RefundBehaviour.cs
@@ -0,0 +1,92 @@
+﻿using System.IO;
+using System.Reflection;
+using ColossalFramework;
+using UnityEngine;
+
+namespace RefundMod
+{
+    public class RefundBehaviour : MonoBehaviour
+    {
+        public bool CanRefund
+        {
+            get { return (!Data.OnlyWhenPaused || _simulationManager.SimulationPaused); }
+        }
+
+        private static RefundBehaviour _instance;
+        public static RefundBehaviour Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    var go = new GameObject();
+                    _instance = go.AddComponent<RefundBehaviour>();
+                    _instance.Init();
+                }
+                    
+                return _instance;
+            }
+        }
+
+        private bool _initialized;
+
+        public RefundModData Data;
+        private RefundModUI _ui;
+
+        private SimulationManager _simulationManager;
+
+        private int _currentDay;
+
+        private FieldInfo _fieldInfo;
+
+        public RefundBehaviour Init()
+        {
+            if (_initialized)
+                return this;
+
+            _simulationManager = Singleton<SimulationManager>.instance;
+            _currentDay = _simulationManager.m_currentGameTime.Day;
+            _fieldInfo = typeof(SimulationManager).GetField("m_buildIndexHistory", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            try
+            {
+                Data = RefundModData.Deserialize();
+            }
+            catch (FileNotFoundException)
+            {
+                Data = new RefundModData();
+            }
+            
+            _ui = gameObject.AddComponent<RefundModUI>();
+            _ui.Init(Data);
+
+            _initialized = true;
+
+            return this;
+        }
+
+        private void ResetBuildIndexHistory()
+        {
+            if (_fieldInfo == null)
+            {
+                _fieldInfo = typeof(SimulationManager).GetField("m_buildIndexHistory", BindingFlags.Instance | BindingFlags.NonPublic);
+            }
+            else
+            {
+                var values = (uint[])_fieldInfo.GetValue(_simulationManager);
+                var len = values.Length;
+                for (var i = 0; i < len; i++)
+                    values[i] = 0; 
+            }
+        }
+
+        void Update()
+        {
+            if (Data.RemoveTimeLimit && _currentDay != _simulationManager.m_currentGameTime.Day)
+            {
+                ResetBuildIndexHistory();
+                _currentDay = _simulationManager.m_currentGameTime.Day;
+            }
+        }
+    }
+}
---
+++ RefundMod/RefundModData.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using UnityEngine;
+
+namespace RefundMod
+{
+    public class RefundModData
+    {
+        private const string Path = "refund.settings.xml";
+
+        public bool Shrunk;
+
+        public bool OnlyWhenPaused;
+        public bool RemoveTimeLimit;
+
+        private float _refundModifier;
+        public float RefundModifier
+        {
+            get { return _refundModifier; }
+            set { _refundModifier = (float)Math.Round(Mathf.Clamp(value, -1, 1), 1); }
+        }
+
+        private float _relocateModifier;
+        public float RelocateModifier
+        {
+            get { return _relocateModifier; }
+            set { _relocateModifier = (float)Math.Round(Mathf.Clamp01(value), 1); }
+        }
+
+        public float X;
+        public float Y;
+
+        public RefundModData(bool shrunk, bool onlyWhenPaused, bool removeTimeLimit, float refundModifier, float relocateModifier, float x, float y)
+        {
+            Shrunk = shrunk;
+            OnlyWhenPaused = onlyWhenPaused;
+            RemoveTimeLimit = removeTimeLimit;
+            RefundModifier = refundModifier;
+            RelocateModifier = relocateModifier;
+            X = x;
+            Y = y;
+        }
+
+        public RefundModData() : this(false, false, false, 0.75f, 0.2f, 60, 20) { }
+
+        public void Serialize()
+        {
+            using (var stream = File.CreateText(Path))
+            {
+                var serializer = new XmlSerializer(typeof(RefundModData));
+                serializer.Serialize(stream, this);
+            }
+        }
+
+        public static RefundModData Deserialize()
+        {
+            RefundModData data;
+
+            using (var stream = File.OpenRead(Path))
+            {
+                var serializer = new XmlSerializer(typeof(RefundModData));
+                data = (RefundModData)serializer.Deserialize(stream);
+            }
+
+            return data;
+        }
+    }
+}
