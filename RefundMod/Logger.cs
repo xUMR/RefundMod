3f194612b0ad2da89f4d558e717588321f13d798
xUMR <umurcem20@gmail.com>
2016-03-26 01:42:42 +0200

    Added logger

---
+++ RefundMod/Logger.cs
@@ -0,0 +1,47 @@
+﻿using ColossalFramework.Plugins;
+
+namespace RefundMod
+{
+    public static class Logger
+    {
+        const bool _enabled = false;
+        const string _label = "(refundmod): ";
+
+#pragma warning disable CS0162 // Unreachable code detected
+        public static void Message(string str)
+        {
+            if (!_enabled) return;
+
+            DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, _label + str);
+        }
+
+        public static void Message(object[] args, string join = " ")
+        {
+            if (!_enabled) return;
+
+            var strBuilder = new System.Text.StringBuilder();
+            foreach (var obj in args)
+            {
+                strBuilder.Append(obj);
+                strBuilder.Append(join);
+            }
+
+            Message(strBuilder.ToString());
+        }
+
+        public static void Warning(string str)
+        {
+            if (!_enabled) return;
+
+            DebugOutputPanel.AddMessage(PluginManager.MessageType.Warning, _label + str);
+        }
+
+        public static void Error(string str)
+        {
+            if (!_enabled) return;
+
+            DebugOutputPanel.AddMessage(PluginManager.MessageType.Error, _label + str);
+        }
+#pragma warning restore CS0162 // Unreachable code detected
+    }
+}
